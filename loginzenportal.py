03ca94cefaba986787b9901c0bb291c82e0c5d8b
naveenrajME <38903452+naveenrajME@users.noreply.github.com>
2022-05-17 22:42:06 +0530

    Update loginzenportal.py

--- loginzenportal.py
+++ loginzenportal.py
@@ -1,13 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
-
-
+'''Class to login in to the zenportal using usewrname and password'''
 class login:
     d = webdriver.Firefox()
     '''Get in to the Zen Class portal and Login'''
-
     def auto_login(self,username, password):
        #Get the URL
         self.d.get('https://www.zenclass.in/')
@@ -16,9 +13,6 @@ class login:
         element=self.d.find_element(by=By.XPATH, value='//*[@id="root"]/div/div/div[1]/div[2]/div/div[1]/form/div[2]/div/input')
         element.send_keys(password) 
         element.send_keys(Keys.RETURN)
-
-
+       
 s = login()
 s.auto_login('naveenraj0705@gmail.com','Naveen@07') # passing the user name and Password
-
-
